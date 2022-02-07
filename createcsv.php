6ace738708675bc54680c3f0763cfbca568bbb10
gregory <uhbifr@rambler.ru>
2022-02-07 15:01:46 +0300

    Update readme

--- createcsv.php
+++ createcsv.php
@@ -1,14 +1,18 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
+echo ini_set('upload_max_filesize', '64M');
+@ini_set('upload_max_filesize', '64M');
 
+print((int)ini_set('post_max_size', '64M') . "<br><br>");
+echo ini_get('upload_max_filesize') . "<br><br>";
 $myList = array();
-$countFields = 150000;
+$countFields = 310000;
 echo $countFields . " rows created<br>";
 
 function genName()
 {
     setlocale(LC_ALL, "ru_RU.UTF-8");
-    $chars = iconv("UTF-8","windows-1251","АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.");
+    $chars = iconv("UTF-8", "windows-1251", "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.");
     $numChars = strlen($chars);
     $length = 15;
     $string = '';
@@ -34,6 +38,7 @@ for ($i = 0; $i < $countFields; $i++) {
     $list = array();
     array_push($list, mt_rand(1, 1000000000), genName());
     array_push($myList, $list);
+    unset($list);
 }
 
 $fp = fopen('file.csv', 'w');
@@ -41,5 +46,5 @@ $fp = fopen('file.csv', 'w');
 foreach ($myList as $fields) {
     fputcsv($fp, $fields);
 }
-
+unset($myList);
 fclose($fp);
