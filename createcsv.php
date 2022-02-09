e3e7cc7b03e380899de26879b5160ce6e20ea073
gregory <uhbifr@rambler.ru>
2022-02-09 23:36:46 +0300

    Modernized CSV-file generation, added  custom value field for generation file

--- createcsv.php
+++ createcsv.php
@@ -1,17 +1,13 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-echo ini_set('upload_max_filesize', '64M');
-@ini_set('upload_max_filesize', '64M');
-
-print((int)ini_set('post_max_size', '64M') . "<br><br>");
-echo ini_get('upload_max_filesize') . "<br><br>";
 $myList = array();
-$countFields = 310000;
-echo $countFields . " rows created<br>";
+$countFields = 100000;
+if (isset($_GET['count']) && is_numeric($_GET['count'])) {
+    $countFields = $_GET['count'];
+} else echo "Custom value not set<br>";
 
 function genName()
 {
-    setlocale(LC_ALL, "ru_RU.UTF-8");
     $chars = iconv("UTF-8", "windows-1251", "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.");
     $numChars = strlen($chars);
     $length = 15;
@@ -23,26 +19,22 @@ function genName()
             $i++;
         }
     }
-
     return iconv("windows-1251", "UTF-8", $string);
-    //return $string;
 }
 
 function interfere()
 {
-    $wrongChars = '`~!@#$%^&*()_|+=?:,';
+    $wrongChars = "`~!@#$%^&*()\"/_|+='?№%<>:,";
     return substr($wrongChars, rand(1, strlen($wrongChars)) - 1, 1);
 }
 
 for ($i = 0; $i < $countFields; $i++) {
-    $list = array();
-    array_push($list, mt_rand(1, 1000000000), genName());
-    array_push($myList, $list);
-    unset($list);
+    array_push($myList, array(mt_rand(1, 1000000000), genName()));
 }
 
-$fp = fopen('file.csv', 'w');
+echo number_format($countFields, 0, '', ' ') . " rows created<br>";
 
+$fp = fopen('file.csv', 'w');
 foreach ($myList as $fields) {
     fputcsv($fp, $fields);
 }
--- readme.md
+++ readme.md
@@ -1,12 +1,18 @@
 # How to use:
-<ol>
- <li>Run createcsv.php for generate random names (in file change $countFields 
-for generate different number of lines). You don't have to use this script if you already have the .csv file </li>
- <li>Open index.html and choose CSV-file.</li>
- <li>Get a report on loaded strings by means of auto download</li>
-</ol>
 
-# **First:**
-<p>Change upload_max_filesize and post_max_size to 34M e.g. in configuration file php.ini</p>
-<p>You need to crete table in your database using by handbook.sql</p>
-<p>Edit file connect.php and paste your connection data</p>
+1. Run _createcsv.php_ for generate random names
+You don't have to use this script if you already have the .csv file. 
+The file will be uploaded to the root directoryю
+> You can set custom value by using createcsv.php?count=**your_value**
+2. Open _index.html_ and choose CSV-file.
+3. Get a report on loaded strings by means of auto download. 
+Rows with wrong characters contained in the name or code field will not be loaded into the table. 
+A description of the error will appear in the column for all invalid rows.
+
+
+# First:
+> Change `upload_max_filesize` and `post_max_size` to **34M** e.g. in configuration file _php.ini_
+
+> You need to crete table in your database using by _handbook.sql_
+
+> Edit file _connect.php_ and paste your connection data
